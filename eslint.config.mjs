b72b132c04c7148ed49618fe5a094e434be6c554
[M-Tha] Suriya Chieblaem <it.Tkf3.04@misumi.co.th>
2025-08-20 17:53:26 +0700

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,27 @@
 // eslint.config.mjs
-import next from 'eslint-config-next'
 import js from '@eslint/js'
 import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import * as tseslint from 'typescript-eslint'
+import nextPlugin from '@next/eslint-plugin-next'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { globalIgnores } from 'eslint/config'
 
 export default [
-  // ignore build artifacts
   globalIgnores(['.next', '.contentlayer', 'node_modules', 'dist']),
-
-  // Next.js base (handles special exports like `metadata`, `viewport`, etc.)
-  ...next,
-
-  // TypeScript recommended
+  js.configs.recommended,
   ...tseslint.configs.recommended,
-
-  // Extra plugins + relaxed rules so builds don’t fail
+  nextPlugin.configs['core-web-vitals'],
   {
     plugins: { 'jsx-a11y': jsxA11y, 'react-refresh': reactRefresh },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
     rules: {
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': [
         'warn',
-        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
-      // If jsx-a11y throws on anchors without content, you can temporarily turn it off:
-      // 'jsx-a11y/anchor-has-content': 'off',
+      // 'jsx-a11y/anchor-has-content': 'off', // uncomment if it blocks you
     },
   },
 ]
