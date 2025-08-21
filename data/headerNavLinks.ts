58b8bb58f3e7f558e337b373fdee5123cb2be0d3
sixmazone-c <sixmazone@gmail.com>
2025-08-21 16:57:40 +0700

    Dev (#7)
    * First Version
    
    * Fixed issue
    
    * chore: add eslint-plugin-react-refresh
    
    * NewVersion
    
    * aa
    
    * aa
    
    * update package
    
    * fix issue
    
    * fix lint
    
    * fix lint
    
    * lint
    
    * lint: keep unused vars warnings
    
    * done
    
    * fix lint
    
    * add page
    
    * remove unessery file
    
    * aa
    
    * update
    
    ---------
    
    Co-authored-by: [M-Tha] Suriya Chieblaem <it.Tkf3.04@misumi.co.th>

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/incidents', title: 'Incidents' },
+  { href: '/checkin', title: 'Check-in' },
+  { href: '/status', title: 'Summary' },
 ]
 
 export default headerNavLinks
